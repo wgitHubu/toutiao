00f565dbafbb955eceb261a8dcb9b1a00fce0d33
wtf <1924140916@qq.com>
2022-09-06 11:20:46 +0800

    feat:项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
+// 目录路径的配置
+const { resolve } = require('path')
+const cpns = resolve(__dirname, 'src/components')
+const views = resolve(__dirname, 'src/views')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // lintOnSave: false
+  // 配置开发服务器，路径别名
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 8888,
+      host: 'localhost'
+    },
+    resolve: {
+      alias: {
+        cpns,
+        views
+      }
+    }
+  }
 })
